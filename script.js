2b08f33e82602ad5e86c7b6fda6cee070e830438
msmannanov <m.s.mannanov@gmail.com>
2026-05-30 11:33:22 +0600

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function toggleMenu() {
     const menu = document.querySelector('.sub-menu');
     menu.classList.toggle('active');
+    console.log("Меню босилди!"); // Бу браузер консолида ишлайдими, йўқми кўрсатади
 }
